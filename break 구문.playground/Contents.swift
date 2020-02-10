99662709af4da0e20c8897ae48c9589b6054f653
김병민 <kpminws@naver.com>
2020-02-10 14:50:29 +0900

    break 구문

---
+++ break 구문.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+/*
+ 
+    * break 구문
+ 
+ */
+
+for row in 1...5 {
+    row
+    if row > 2 {
+        break
+    }
+    print("\(row)was executed")
+} //1was executed 2was executed
+
+
+/*
+ 
+        * break 구문의 응용
+ 
+ */
